4df3a470ef4e4a5294a961771e67f05272adfd09
Osama <oaboalwafa75@gmail.com>
2026-02-22 22:48:14 +0300

    Update system_info.sh

--- system_info.sh
+++ system_info.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Get system information
 EXECUTED_BY=$(whoami)
 HOSTNAME=$(hostname)
 SERVER_IP=$(hostname -I | awk '{print $1}')
 
-# Public IP with fallback
 PUBLIC_IP=$(curl -s --connect-timeout 5 https://ifconfig.me || echo "Not Available")
 
 OS_TYPE_VERSION=$(grep "PRETTY_NAME" /etc/os-release | cut -d'=' -f2 | tr -d '"')
@@ -16,7 +14,6 @@ SERVER_TIME=$(date "+%a %b %d %T %p %Z %Y")
 TIMEZONE=$(timedatectl | grep "Time zone" | awk '{print $3 " (" $4 " " $5}')
 UPTIME=$(uptime -p | sed 's/up //')
 
-# Resource Usage
 TOTAL_MEMORY=$(free -h | awk '/^Mem:/ {print $2}')
 MEMORY_USAGE=$(free -h | awk '/^Mem:/ {print $3 " / " $2}')
 SWAP_USAGE=$(free -h | awk '/^Swap:/ {print $3 " / " $2}')
